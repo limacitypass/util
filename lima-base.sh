30e2ffd75546650da1780839845593150a6825a4
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2019-03-25 02:03:32 -0500

    added node

--- lima-base.sh
+++ lima-base.sh
@@ -3,8 +3,13 @@ set -e
 
 sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive UCF_FORCE_CONFFNEW=YES apt upgrade -yq
+# Installing docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker ubuntu
+# Installing docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+# Installing node@10
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt install -y nodejs
