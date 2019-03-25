04afa26f4b722bdc05e4f0ca0eed7ace3e92dfca
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2019-03-25 01:44:34 -0500

    added lima base

---
+++ lima-base.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+sudo apt update
+sudo DEBIAN_FRONTEND=noninteractive UCF_FORCE_CONFFNEW=YES apt upgrade -yq
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker ubuntu
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
